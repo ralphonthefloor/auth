85183aa2e2c93841d3e189a28f1c2739cf272135
Matthew <kinnaird.matthew@gmail.com>
2022-02-23 10:33:12 -0500

    bcrypt/salting

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -33,16 +34,18 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req,res){
-const newUser = new User({
-  email: req.body.username,
-  password: md5(req.body.password)
-});
-newUser.save(function(err){
-  if(err){
-    console.log(err);
-  } else {
-    res.render("secrets");
-  }
+bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+  const newUser = new User({
+    email: req.body.username,
+    password: hash
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
 });
 });
 
@@ -53,12 +56,11 @@ app.post("/login", function(req,res){
     if(err){
       console.log(err);
     } else {
-      if(foundUser.password === md5(password)){
-        res.render("secrets");
-      } else {
-        console.log("User credentials invalid");
-        res.render("home");
-      }
+      bcrypt.compare(password, foundUser.password, function(err,result){
+        if(result === true){
+          res.render("secrets");
+        }
+      });
       }
     });
   });
