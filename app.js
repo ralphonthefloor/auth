e63a6097bc1341bbea2c207ffe78d843da2c0599
Matthew <kinnaird.matthew@gmail.com>
2022-02-22 20:35:25 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://" + process.env.DB_HOST + ":" + process.env.DB_PORT + "/" + process.env.DB_NAME);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+//binds the encrypt object from mongoose-encrypt to the mongoose Schema instance
+//and is used whenever find or save methods are used on that Schema instance
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req,res){
+  res.render("home");
+});
+
+app.get("/login", function(req,res){
+  res.render("login");
+});
+
+app.get("/register", function(req,res){
+  res.render("register");
+});
+
+app.post("/register", function(req,res){
+const newUser = new User({
+  email: req.body.username,
+  password: req.body.password
+});
+newUser.save(function(err){
+  if(err){
+    console.log(err);
+  } else {
+    res.render("secrets");
+  }
+});
+});
+
+app.post("/login", function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email: username}, function (err, foundUser){
+    if(err){
+      console.log(err);
+    } else {
+      if(foundUser.password === password){
+        res.render("secrets");
+      } else {
+        console.log("User credentials invalid");
+        res.render("home");
+      }
+      }
+    });
+  });
+
+
+app.listen(3000, function() {
+  console.log("Server is listening on port 3000")
+});
